04ff16527bb108b87af147f6aceea7ac512b6b17
joye-seo <104261048+joye-seo@users.noreply.github.com>
2022-05-19 10:13:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/lotto_practice_20220519/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lotto_practice_20220519
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
